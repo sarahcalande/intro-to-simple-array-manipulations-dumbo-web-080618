59d4255749e0ee7b388ae1a32026d980f4e120c7
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 14:21:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa.push(next_country)
 end
 
-def using_unshift(array)
-  array.unshift()
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  array.unshift(new_neighborhood)
 end
 
 def using_pop(array)
