9eb87cf0b985531c5bbb13afcbaeba9f3a55ee52
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 14:20:20 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa.push(next_country)
+end
+
+def using_unshift(array)
+  array.unshift()
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array)
+  array.delete()
+end
